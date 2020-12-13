f0edc20476f239fb0007616d631a70a577e19011
Родион Котов <rodtdt@yandex.ru>
2020-12-13 22:24:20 +0700

    build_rpn has bad syntax

--- calc/main.c
+++ calc/main.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS 
+#define  _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 #include<malloc.h>
 #include<string.h>
@@ -29,187 +29,184 @@ double parsing_str_to_float(char* str);
 double calculate_by_rpn(Symbol_data* arr);
 double calculate_by_string(char* string);
 
-
+int check_few_point(char* str) {
+	int count = 0;
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == '.')
+			count++;
+		if (count > 1)
+			return 1;
+	}
+	return 0;
+}
 Symbol_data* parsing_str_to_array_symbol(char* str) {
-
+	unsigned int i = -1;
+	unsigned int count = 0;
+	unsigned int check_num = 0;
 
 	Symbol_data* arr_exp = malloc(sizeof(Symbol_data) * MAX_LEN_EXP);
 
-	if (arr_exp == NULL) {
-		return NULL;
-	}
-	else {
-		unsigned int i = -1;
-		unsigned int count = 0;
-		unsigned int check_num = 0;
-		int flag_error = 0;
+	char alf_operation[] = "\n *-/+()";
+	char alf_operand[] = "0123456789";
+	arr_exp[count].type = END;
+	do {
+		i++;
+		if (strchr(alf_operation, str[i]) != NULL) {
+			if (check_num > 0) {
+				//Exception: Float haven`t float part 
+				if (arr_exp[count].data[check_num - 1] == '.' ||
+					arr_exp[count].data[0] == '.' ||
+					check_few_point(arr_exp[count].data) == 1) {
+					arr_exp[count + 1].type = END;
+					free_array_symbol(arr_exp);
 
-		char* buffer = NULL;
-
-		char alf_operation[] = "\n *-/+()";
-		char alf_operand[] = "0123456789";
-
-		while (str[i] != '\n' && flag_error == 0) {
-			i++;
-			if (strchr(alf_operation, str[i]) != NULL) {
-				if (check_num > 0) {
-					//Exception: Float haven`t float part 
-					if (buffer[check_num - 1] == '.') {
-						flag_error = 1;
-						break;
-					}
-					arr_exp[count].data = buffer;
-					arr_exp[count].type = OPERAND;
-					count++;
-
-					check_num = 0;
+					return NULL;
 				}
-				if (str[i] != ' ') {
-					if (str[i] == '\n')
-						arr_exp[count].type = END;
-					else {
-						arr_exp[count].type = OPERATION;
-						buffer = malloc(sizeof(char) * 20);
+				count++;
+				check_num = 0;
+			}
+			if (str[i] != ' ') {
+				//buffer = (char*)malloc(sizeof(char) * 20);
+				arr_exp[count].data = (char*)malloc(sizeof(char) * 20);
+				arr_exp[count].data[0] = str[i];
+				arr_exp[count].data[1] = '\0';
+				arr_exp[count].type = OPERATION;
+				count++;
+			}
+			arr_exp[count].type = END;
+		}
+		else
+			if (strchr(alf_operand, str[i]) != NULL) {
+				if (check_num == 0) {
+					arr_exp[count].data = (char*)malloc(sizeof(char) * 20);
+					arr_exp[count].type = OPERAND;
+					arr_exp[count].data[0] = (char)str[i];
+					arr_exp[count].data[1] = '\0';
+					check_num = 1;
+				}
+				else {
+					arr_exp[count].data[check_num] = str[i];
+					arr_exp[count].data[check_num + 1] = '\0';
 
-						buffer[0] = (char)str[i];
-						buffer[1] = '\0';
-						arr_exp[count].data = buffer;
-					}count++;
-
+					check_num++;
 				}
 			}
-			else
-				if (strchr(alf_operand, str[i]) != NULL) {
-					if (check_num == 0) {
-						//Exception: Float haven`t integer part 
-						if (str[i] == '.') {
-							flag_error = 1;
-							break;
-						}
-						buffer = malloc(sizeof(char) * 20);
+			else {
+				if (check_num > 0)
+					arr_exp[count + 1].type = END;
+				free_array_symbol(arr_exp);
+				return NULL;
+			}
+	} while (str[i] != '\n');
 
-						buffer[0] = (char)str[i];
-						buffer[1] = '\0';
-
-						check_num = 1;
-					}
-					else {
-						//Exception: Float have more then 1 points 
-						if (str[i] == '.' && strchr(buffer, alf_operand[0]) != NULL) {
-							if (buffer != NULL) {
-								flag_error = 1;
-								break;
-							}
-						}
-						buffer[check_num] = str[i];
-						buffer[check_num + 1] = '\0';
-
-						check_num++;
-					}
-				}
-				else
-				{
-					//Exception: String have foreign symbol
-					flag_error = 1;
-					break;
-				}
-		}
-		// exception handling 
-		if (flag_error == 1) {
-			if (buffer != NULL)
-				free(buffer);
-			arr_exp[count].type = END;
-			free_array_symbol(arr_exp);
-			return NULL;
-		}
-		return arr_exp;
-	}
+	return arr_exp;
 }
 Symbol_data* free_array_symbol(Symbol_data* arr) {
 	if (arr != NULL) {
 		int i = 0;
 		while (arr[i].type != END) {
-			if (arr[i].data != NULL)
-				free(arr[i].data);
+			free(arr[i].data);
 			i++;
 		}
-
 		free(arr);
 		return NULL;
 	}
 	return NULL;
 }
+
 Symbol_data* build_rpn(Symbol_data* arr) {
-	int i = 0;
+	int i = -1;
 	int situation_result = 0;
 	int situation_stack = -1;
 	int flag_operand = 0;
-
 	Symbol_data* result = malloc(sizeof(Symbol_data) * MAX_LEN_EXP);
 	Symbol_data* stack = malloc(sizeof(Symbol_data) * MAX_LEN_EXP);
-
-	while (arr[i].type != END) {
-
-		if (arr[i].type == OPERAND) {
-			result[situation_result++] = arr[i];
-			flag_operand = 1;
-		}
-		else {
-
-			if (arr[i].data[0] == '(') {
-				stack[++situation_stack] = arr[i];
-				flag_operand = 0;
-			}
-
-			if (arr[i].data[0] == ')') {
-				int flag = 0;
-				while (situation_stack >= 0 && stack[situation_stack].data[0] != '(') {
-					result[situation_result++] = stack[situation_stack];
-					flag = 1;
-					situation_stack--;
-				}
-
-				if (situation_stack == -1 || (flag == 0 && flag_operand == 0)) {
-					break;
-				}
-				situation_stack--;
-			}
-
-			if (arr[i].data[0] == '*' || arr[i].data[0] == '/') {
-				while (situation_stack >= 0 && stack[situation_stack].data[0] != '('
-					&& (stack[situation_stack].data[0] == '*' || stack[situation_stack].data[0] == '/')) {
-					result[situation_result++] = stack[situation_stack];
-					situation_stack--;
-				}
-				stack[++situation_stack] = arr[i];
-			}
-
-			if (arr[i].data[0] == '+' || arr[i].data[0] == '-') {
-				while (situation_stack >= 0 && stack[situation_stack].data[0] != '(') {
-					result[situation_result++] = stack[situation_stack];
-					situation_stack--;
-				}
-				stack[++situation_stack] = arr[i];
-			}
-
-		}
-		i++;
+	if (result == NULL) {
+		return NULL;
 	}
-	if (arr[i].type == END) {
-		while (situation_stack >= 0)
+	else if (stack == NULL) {
+		free(result);
+		return NULL;
+	}
+	else {
+		do
 		{
-			result[situation_result++] = stack[situation_stack];
-			situation_stack--;
-		}
-		result[situation_result++] = arr[i];
-		free(stack);
-		return result;
-	}
-	free(stack);
-	free(result);
-	return NULL;
+			i++;
+			if (arr[i].type == OPERAND) {
+				result[situation_result++] = arr[i];
+				flag_operand = 1;
+			}
+			else
+				if (arr[i].type == OPERATION) {
 
+					if (arr[i].data[0] == '(') {
+						stack[++situation_stack] = arr[i];
+						flag_operand = 0;
+					}
+
+					if (arr[i].data[0] == ')') {
+						int flag = 0;
+						while (situation_stack >= 0 && stack[situation_stack].data[0] != '(') {
+							if (stack[situation_stack].data[0] != '(')
+							{
+								result[situation_result++] = stack[situation_stack];
+								flag = 1;
+							}
+							situation_stack--;
+						}
+						if (situation_stack == -1 || (flag == 0 && flag_operand == 0)) {
+							free(stack);
+							free(result);
+							return NULL;
+						}
+						situation_stack--;
+					}
+
+					if (arr[i].data[0] == '*' || arr[i].data[0] == '/') {
+						while (situation_stack >= 0 && stack[situation_stack].data[0] != '(') {
+							if (stack[situation_stack].data[0] == '*' || stack[situation_stack].data[0] == '/') {
+								result[situation_result++] = stack[situation_stack];
+								situation_stack--;
+							}
+							else {
+								break;
+							}
+						}
+						stack[++situation_stack] = arr[i];
+					}
+
+					if (arr[i].data[0] == '+' || arr[i].data[0] == '-') {
+						while (situation_stack >= 0 && stack[situation_stack].data[0] != '(') {
+							result[situation_result++] = stack[situation_stack];
+							situation_stack--;
+						}
+						stack[++situation_stack] = arr[i];
+					}
+
+				}
+			if (arr[i].type == END) {
+				while (situation_stack >= 0)
+				{
+					if (stack[situation_stack].data[0] == '(')
+					{
+						free(result);
+						free(stack);
+						return NULL;
+					}
+					result[situation_result++] = stack[situation_stack];
+					situation_stack--;
+				}
+				result[situation_result++] = arr[i];
+				free(stack);
+				return result;
+			}
+
+		} while (arr[i].type != END);
+	}
+	return NULL;
 }
+
+
 
 double parsing_str_to_float(char* str) {
 	double result = 0;
@@ -312,5 +309,6 @@ int main() {
 
 	}
 	free(str_expression);
+	//_CrtDumpMemoryLeaks();
 	return 0;
 }
