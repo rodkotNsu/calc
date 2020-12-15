565b0312e3b6d0a34121b97b8ceec6c51d6d4a57
Родион Котов <rodtdt@yandex.ru>
2020-12-15 20:30:26 +0700

    update main

--- calc/main.c
+++ calc/main.c
@@ -7,6 +7,9 @@
 #define MAX_LEN_EXP 1100
 #define ERROR_SYNTAX_ERROR -9999
 #define ERROR_DIVISION_BY_ZERO -9998
+ 
+#define ALF_OPERATION "\n *-/+()"
+#define ALF_OPERAND "0123456789"
 
 enum Syntax_type
 {
@@ -17,6 +20,7 @@ enum Syntax_type
 struct Symbol_data
 {
 	char* data;
+	int value;
 	enum Syntax_type type;
 };
 typedef struct Symbol_data Symbol_data;
@@ -25,48 +29,27 @@ Symbol_data* free_array_symbol(Symbol_data* arr);
 Symbol_data* parsing_str_to_array_symbol(char* str);
 Symbol_data* build_rpn(Symbol_data* arr);
 
-double parsing_str_to_float(char* str);
 double calculate_by_rpn(Symbol_data* arr);
 double calculate_by_string(char* string);
+int get_priority(char* operator);
 
-int check_few_point(char* str) {
-	int count = 0;
-	for (int i = 0; i < strlen(str); i++) {
-		if (str[i] == '.')
-			count++;
-		if (count > 1)
-			return 1;
-	}
-	return 0;
-}
 Symbol_data* parsing_str_to_array_symbol(char* str) {
 	unsigned int i = -1;
 	unsigned int count = 0;
 	unsigned int check_num = 0;
 
 	Symbol_data* arr_exp = malloc(sizeof(Symbol_data) * MAX_LEN_EXP);
-
-	char alf_operation[] = "\n *-/+()";
-	char alf_operand[] = "0123456789";
+	char buffer[20] = { 0 };
 	arr_exp[count].type = END;
 	do {
 		i++;
-		if (strchr(alf_operation, str[i]) != NULL) {
+		if (strchr(ALF_OPERATION, str[i]) != NULL) {
 			if (check_num > 0) {
-				//Exception: Float haven`t float part 
-				if (arr_exp[count].data[check_num - 1] == '.' ||
-					arr_exp[count].data[0] == '.' ||
-					check_few_point(arr_exp[count].data) == 1) {
-					arr_exp[count + 1].type = END;
-					free_array_symbol(arr_exp);
-
-					return NULL;
-				}
+				if (sscanf(buffer, "%d", &arr_exp[count].value))
 				count++;
 				check_num = 0;
 			}
-			if (str[i] != ' ') {
-				//buffer = (char*)malloc(sizeof(char) * 20);
+			if (str[i] != ' ' && str[i] != '\n') {
 				arr_exp[count].data = (char*)malloc(sizeof(char) * 20);
 				arr_exp[count].data[0] = str[i];
 				arr_exp[count].data[1] = '\0';
@@ -76,17 +59,17 @@ Symbol_data* parsing_str_to_array_symbol(char* str) {
 			arr_exp[count].type = END;
 		}
 		else
-			if (strchr(alf_operand, str[i]) != NULL) {
+			if (strchr(ALF_OPERAND, str[i]) != NULL) {
 				if (check_num == 0) {
-					arr_exp[count].data = (char*)malloc(sizeof(char) * 20);
+					 
 					arr_exp[count].type = OPERAND;
-					arr_exp[count].data[0] = (char)str[i];
-					arr_exp[count].data[1] = '\0';
+					buffer[0] = (char)str[i];
+					buffer[1] = '\0';
 					check_num = 1;
 				}
 				else {
-					arr_exp[count].data[check_num] = str[i];
-					arr_exp[count].data[check_num + 1] = '\0';
+					buffer[check_num] = str[i];
+					buffer[check_num + 1] = '\0';
 
 					check_num++;
 				}
@@ -105,7 +88,8 @@ Symbol_data* free_array_symbol(Symbol_data* arr) {
 	if (arr != NULL) {
 		int i = 0;
 		while (arr[i].type != END) {
-			free(arr[i].data);
+			if (arr[i].type == OPERATION)
+				free(arr[i].data);
 			i++;
 		}
 		free(arr);
@@ -113,129 +97,80 @@ Symbol_data* free_array_symbol(Symbol_data* arr) {
 	}
 	return NULL;
 }
-
+int get_priority(char* operator) {
+	switch (operator[0])
+	{
+	case'+':case'-':return 1; break;
+	case'*':case'/':return 2; break;
+	case'(':return 0; break;
+	case')':return -1; break;
+	default:
+		return -2;
+		break;
+	}
+	return -2;
+}
 Symbol_data* build_rpn(Symbol_data* arr) {
-	int i = -1;
+	int i = 0;
 	int situation_result = 0;
 	int situation_stack = -1;
 	int flag_operand = 0;
+
 	Symbol_data* result = malloc(sizeof(Symbol_data) * MAX_LEN_EXP);
 	Symbol_data* stack = malloc(sizeof(Symbol_data) * MAX_LEN_EXP);
-	if (result == NULL) {
-		return NULL;
-	}
-	else if (stack == NULL) {
-		free(result);
-		return NULL;
-	}
-	else {
-		do
-		{
-			i++;
-			if (arr[i].type == OPERAND) {
-				result[situation_result++] = arr[i];
-				flag_operand = 1;
-			}
-			else
-				if (arr[i].type == OPERATION) {
 
-					if (arr[i].data[0] == '(') {
-						stack[++situation_stack] = arr[i];
-						flag_operand = 0;
-					}
-
-					if (arr[i].data[0] == ')') {
-						int flag = 0;
-						while (situation_stack >= 0 && stack[situation_stack].data[0] != '(') {
-							if (stack[situation_stack].data[0] != '(')
-							{
-								result[situation_result++] = stack[situation_stack];
-								flag = 1;
-							}
-							situation_stack--;
-						}
-						if (situation_stack == -1 || (flag == 0 && flag_operand == 0)) {
-							free(stack);
-							free(result);
-							return NULL;
-						}
-						situation_stack--;
-					}
-
-					if (arr[i].data[0] == '*' || arr[i].data[0] == '/') {
-						while (situation_stack >= 0 && stack[situation_stack].data[0] != '(') {
-							if (stack[situation_stack].data[0] == '*' || stack[situation_stack].data[0] == '/') {
-								result[situation_result++] = stack[situation_stack];
-								situation_stack--;
-							}
-							else {
-								break;
-							}
-						}
-						stack[++situation_stack] = arr[i];
-					}
-
-					if (arr[i].data[0] == '+' || arr[i].data[0] == '-') {
-						while (situation_stack >= 0 && stack[situation_stack].data[0] != '(') {
-							result[situation_result++] = stack[situation_stack];
-							situation_stack--;
-						}
-						stack[++situation_stack] = arr[i];
-					}
-
-				}
-			if (arr[i].type == END) {
-				while (situation_stack >= 0)
-				{
-					if (stack[situation_stack].data[0] == '(')
-					{
-						free(result);
-						free(stack);
-						return NULL;
-					}
+	while (arr[i].type != END) {
+		if (arr[i].type == OPERAND) {
+			result[situation_result++] = arr[i];
+			flag_operand = 1;
+		}
+		else {
+			if (get_priority(arr[i].data) != get_priority("(")) {
+				while (situation_stack >= 0 &&
+					get_priority(arr[i].data) <= get_priority(stack[situation_stack].data)
+					&& get_priority(stack[situation_stack].data) != get_priority("(")) {
 					result[situation_result++] = stack[situation_stack];
 					situation_stack--;
 				}
-				result[situation_result++] = arr[i];
-				free(stack);
-				return result;
+				if (situation_stack >= 0 && get_priority(stack[situation_stack].data) == get_priority("(")
+					&& get_priority(arr[i].data)==get_priority(")")) {
+					if (flag_operand != 1) {
+						break;
+					}
+					else {
+						i++;
+						situation_stack--;
+						continue;
+					}
+				}
 			}
-
-		} while (arr[i].type != END);
-	}
-	return NULL;
-}
-
-
-
-double parsing_str_to_float(char* str) {
-	double result = 0;
-	double pow_10 = 1;
-	char* point = strchr(str, '.');
-	if (point != NULL)
-		pow_10 = pow(10, (double)(point - &str[0] - 1));
-	else
-		pow_10 = pow(10, (double)(strlen(str) - 1));
-	int i = 0;
-	while (str[i] != '\0') {
-		if (str[i] != '.')
-		{
-			result += (str[i] - '0') * pow_10;
-			pow_10 /= 10.0;
+			stack[++situation_stack] = arr[i];
+			flag_operand = 0;
 		}
 		i++;
 	}
-	return result;
+	if (arr[i].type == END) {
+		while (situation_stack >= 0)
+		{
+			result[situation_result++] = stack[situation_stack];
+			situation_stack--;
+		}
+		result[situation_result++] = arr[i];
+		free(stack);
+		return result;
+	}
+	free(stack);
+	free(result);
+	return NULL;
 }
 double calculate_by_rpn(Symbol_data* arr) {
 	double stack[MAX_LEN_EXP];
 	int situation_stack = -1;
 	int i = 0;
 	while (arr[i].type != END) {
-		if (arr[i].type == OPERAND)
-		{
+		if (arr[i].type == OPERAND){
 			situation_stack++;
-			stack[situation_stack] = parsing_str_to_float(arr[i].data);
+			stack[situation_stack] = arr[i].value;
 		}
 		else if (arr[i].type == OPERATION) {
 			if (situation_stack < 1)
@@ -271,10 +206,7 @@ double calculate_by_rpn(Symbol_data* arr) {
 	if (situation_stack == 0) {
 		return  stack[situation_stack];
 	}
-
 	return ERROR_SYNTAX_ERROR;
-
-
 }
 double calculate_by_string(char* string) {
 	Symbol_data* symbol_arr = parsing_str_to_array_symbol(string);
@@ -290,10 +222,8 @@ double calculate_by_string(char* string) {
 	free_array_symbol(symbol_arr);
 	return result;
 }
-
 int main() {
 	char* str_expression = (char*)malloc(sizeof(char) * MAX_LEN_EXP);
-
 	if (fgets(str_expression, MAX_LEN_EXP, stdin)) {
 		double result = calculate_by_string(str_expression);
 		if (result == ERROR_SYNTAX_ERROR) {
@@ -302,13 +232,10 @@ int main() {
 		else if (result == ERROR_DIVISION_BY_ZERO) {
 			printf("division by zero");
 		}
-		else
-		{
+		else{
 			printf("%d", (int)result);
 		}
-
 	}
 	free(str_expression);
-	//_CrtDumpMemoryLeaks();
 	return 0;
 }
